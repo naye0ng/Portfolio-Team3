86c5409e72b8bcc6244b897c3cc48d909a1d13f1
Park Haewon <hanaluana23@gmail.com>
2019-07-09 16:44:08 +0900

    Fix current User function

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -135,15 +135,6 @@ export default {
   },
   curUser() {
     var user = firebase.auth().currentUser;
-    // if (user != null) {
-    //   user.providerData.forEach(function (profile) {
-    //     console.log("Sign-in provider: " + profile.providerId);
-    //     console.log("  Provider-specific UID: " + profile.uid);
-    //     console.log("  Name: " + profile.displayName);
-    //     console.log("  Email: " + profile.email);
-    //     console.log("  Photo URL: " + profile.photoURL);
-    //   });
-    // }
     return user;
   }
 }
