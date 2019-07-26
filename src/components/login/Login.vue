79463eecaa04b7ecf6d3119d6bc1afed45866190
Park Haewon <hanaluana23@gmail.com>
2019-07-26 10:22:45 +0900

    Fix button

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,41 +1,42 @@
 <template>
-  <v-layout align-center justify-center row wrap elevation-5 style="height:100%;" white px-4 pb-4>
+  <v-container py-0 style="background-color:#fff;">
+    <v-layout align-center justify-center row wrap style="height:100%;" white px-4 pb-4>
+      <!-- 로그아웃 -->
+      <v-flex xs12 text-xs-center v-if="user" my-3>
+        <Logout></Logout>
+      </v-flex>
 
-    <!-- 로그아웃 -->
-    <v-flex xs12 text-xs-center v-if="user" my-3>
-      <Logout></Logout>
-    </v-flex>
+      <!-- SNS 로그인 -->
+      <SnsLogin v-if="!user"></SnsLogin>
 
-    <!-- SNS 로그인 -->
-    <SnsLogin v-if="!user"></SnsLogin>
+      <!-- 이메일 로그인 -->
+      <v-flex xs12 text-xs-center v-if="!user">
+        <UserLogin></UserLogin>
+      </v-flex>
 
-    <!-- 이메일 로그인 -->
-    <v-flex xs12 text-xs-center v-if="!user">
-      <UserLogin></UserLogin>
-    </v-flex>
+      <!-- 비밀번호 찾기 -->
+      <v-flex mt-1 xs4 offset-xs7 text-xs-center v-if="!user">
+        <FindLost></FindLost>
+      </v-flex>
+      <v-flex xs12 pt-3 pb-3 v-if="!user">
+        <hr style="height:0.8px; border:none; color:#D3D3D3; background-color:#D3D3D3; " />
+      </v-flex>
 
-    <!-- 비밀번호 찾기 -->
-    <v-flex mt-1 xs4 offset-xs7 text-xs-center v-if="!user">
-      <FindLost></FindLost>
-    </v-flex>
-    <v-flex xs12 pt-3 pb-3 v-if="!user">
-      <hr style="height:0.8px; border:none; color:#D3D3D3; background-color:#D3D3D3; ">
-    </v-flex>
+      <!-- 회원가입 -->
+      <v-flex xs12 text-xs-center v-if="!user">
+        <Register></Register>
+      </v-flex>
 
-    <!-- 회원가입 -->
-    <v-flex xs12 text-xs-center v-if="!user">
-      <Register></Register>
-    </v-flex>
-
-    <!-- 익명로그인 -->
-    <v-flex xs12 text-xs-center v-if="!user">
-      <v-layout row justify-center>
-        <v-btn color="#7A7A7A" dark v-on:click="loginAnno" style="width:80%;">
-          <v-icon size="25" class="mr-2">fa-question-circle</v-icon>비회원 로그인
-        </v-btn>
-      </v-layout>
-    </v-flex>
-  </v-layout>
+      <!-- 익명로그인 -->
+      <v-flex xs12 text-xs-center v-if="!user">
+        <v-layout row justify-center>
+          <v-btn color="#7A7A7A" dark v-on:click="loginAnno" style="width:80%;">
+            <v-icon size="25" class="mr-2">fa-question-circle</v-icon>비회원 로그인
+          </v-btn>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -54,21 +55,21 @@ export default {
     UserLogin,
     FindLost,
     Logout,
-    SnsLogin,
+    SnsLogin
   },
   methods: {
     async loginAnno() {
       const result = await LoginService.loginAnno();
-      this.$store.commit('pushWebLog','anonymous')
-    },
-  },
-  mounted() {
-    this.$store.dispatch("checkUserStatus")
-  },
-  computed: {
-    user(){
-      return this.$store.getters.getUser;
+      this.$store.commit("pushWebLog", "anonymous");
     }
   },
+  mounted() {
+    this.$store.dispatch("checkUserStatus");
+  },
+  computed: {
+    user() {
+      return this.$store.getters.getUser;
+    }
+  }
 };
 </script>
