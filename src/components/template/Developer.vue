462549b314cfa7ddbb0734e82f5351cd070541d2
임현아 <ahlim721@gmail.com>
2019-07-26 09:43:20 +0900

    Fix problem about developers v-for

--- src/components/template/Developer.vue
+++ src/components/template/Developer.vue
@@ -59,7 +59,7 @@
             </v-layout>
             <v-container fluid grid-list-lg>
               <v-layout row wrap>
-                <v-flex v-for="c in capa" xs6>
+                <v-flex v-for="c in capa" :key="c.title" xs6>
                   <span><strong>{{c.title}}</strong></span>
                   <v-progress-linear :value="`${c.prog}`" :color="bgcolor"></v-progress-linear>
                 </v-flex>
