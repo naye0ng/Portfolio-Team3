fbb39b70e4399344be9312cbd84953b106786544
naye0ng <nayeong_e@naver.com>
2019-07-29 11:13:38 +0900

    Add admin page and access change function

--- src/components/admin/AdminTable.vue
+++ src/components/admin/AdminTable.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-simple-table fixed-header height="300px">
+  <table class='user-table'>
     <thead>
       <tr>
         <th class="text-left">Name</th>
         <th class="text-left">Email</th>
         <th class="text-left">Access Level</th>
+        <th class="text-left"></th>
         <th class="text-left"></th>
       </tr>
     </thead>
@@ -12,29 +13,22 @@
       <tr v-for="user in users" :key="user">
         <td class="table-content">{{ user.name }}</td>
         <td class="table-content">{{ user.email }}</td>
-        <td class="table-content" style="text-align:center">{{ user.accessLevel }}</td>
+        <td class="table-content" v-if="user.accessLevel == 0">Guest</td>
+        <td class="table-content" v-if="user.accessLevel == 1">Member</td>
+        <td class="table-content" v-if="user.accessLevel == 2">Admin</td>
         <td class="table-content">
-          <v-radio-group v-model="row" row>
-            <v-radio
-                label="Guest"
-                color="primary"
-                value="0"
-              ></v-radio>
-            <v-radio
-                label="Member"
-                color="success"
-                value="1"
-              ></v-radio>
-            <v-radio
-                label="Admin"
-                color="error"
-                value="2"
-              ></v-radio>
-          </v-radio-group>
+          <form>
+            <input type="radio" :name="user.email.split('@')[0]" value="0" :checked="user.accessLevel == 0" /> Guest
+            <input type="radio" :name="user.email.split('@')[0]" value="1" :checked="user.accessLevel == 1" /> Member
+            <input type="radio" :name="user.email.split('@')[0]" value="2" :checked="user.accessLevel == 2" /> Admin
+          </form>
+        </td>
+        <td class="table-content">        
+          <button v-on:click="changeAccessLevel(user.email.split('@')[0])" class='lavel-btn'>변경</button>
         </td>
       </tr>
     </tbody>
-  </v-simple-table>
+  </table>
 </template>
 <script>
 import firebase from "firebase";
@@ -49,8 +43,15 @@ export default {
       firebase.database().ref().child("user").on("value", snapshot => {
         var user = snapshot.val();
         this.users = Object.values(user)
-        console.log(this.users)
       })
+    },
+    changeAccessLevel(targetName){
+      var el = document.getElementsByName(targetName)
+      for(var i=0; i<el.length;i++){
+        if(el[i].checked == true){
+          firebase.database().ref().child("user").child(targetName).child("accessLevel").set(el[i].value)
+        }
+      }
     }
   },
   mounted() {
@@ -62,5 +63,33 @@ export default {
 td.table-content{
   padding: 0 10px;
   margin: 0 10px;
+  text-align:center;
+}
+.user-table{
+  width:100%;
+  border-collapse: collapse;
+  border-spacing: 2px;
+  border-bottom: 2px solid #dee2e6;
+}
+.user-table thead th{
+    padding: .75rem;
+    vertical-align: middle;
+    border-top: 2px solid #dee2e6;
+    border-bottom: 2px solid #dee2e6;
+}
+.user-table td {
+    padding: .75rem;
+    vertical-align: middle;
+    border-bottom: 1px solid #dee2e6;
+}
+.lavel-btn{
+  border-radius:5px;
+  background-color: #dee2e6;
+  color: #181818;
+  padding: 3px 7px;
+}
+.lavel-btn:hover{
+  background-color: #ec407a;
+  color: #fff;
 }
 </style>
