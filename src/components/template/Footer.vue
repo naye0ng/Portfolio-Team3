262a6afdcef4bf9aa91ed4aeaf2344c64f20a83a
Park Haewon <hanaluana23@gmail.com>
2019-07-30 12:16:27 +0900

    Modify footer ui

--- src/components/template/Footer.vue
+++ src/components/template/Footer.vue
@@ -21,11 +21,11 @@
       <v-flex xs4 py-3 class="footer-el text-xs-center">
         <div id="google_translate_element"></div>
       </v-flex>
-      <v-flex xs4 py-3 class="footer-el text-xs-center " style="color:#fff; cursor:pointer;" align-self-center>
+      <v-flex xs4 py-3 class="footer-el text-xs-center " style="color:#fff;" align-self-center>
         <div style="display:inline-block;">&copy;2019 — </div><!--<a href="https://lab.ssafy.com/naye0ng/webmobile-sub2">SSAFY Team3</a>-->
         <img
         @click="openWindow('https://lab.ssafy.com/naye0ng/webmobile-sub2')"
-        src="../../assets/logodark.png" style="height:2.5rem; display:inline-block"><img>
+        src="../../assets/logodark.png" style="height:2.5rem; display:inline-block; cursor:pointer;"><img>
       </v-flex>
       <v-flex xs4 py-3 class="footer-el text-xs-center" style="line-height: 2.5rem; color:#fff;">
         <weather/>
