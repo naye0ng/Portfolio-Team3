b91163162a7d5385218c63d21b6dfa7765ff0809
Park Haewon <hanaluana23@gmail.com>
2019-08-01 15:14:27 +0900

    Modify like

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -270,6 +270,88 @@ export default {
         })
       })
   },
+  async deletePortLike(portid, useremail){
+    await firestore.collection('portfolios').doc(portid).collection('likeList').doc(useremail).delete();
+    return true;
+  },
+  async addPortLike(portid, useremail){
+    await firestore.collection('portfolios').doc(portid).collection('likeList').doc(useremail).set({
+      created_at : firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    return true;
+  },
+  checkPortLike(portid, useremail){
+    const likes = firestore.collection('portfolios').doc(portid).collection('likeList').doc(useremail);
+    return likes.get()
+    .then(doc => {
+      if (doc.exists){
+        return true;
+      }
+      else{
+        return false;
+      }
+    })
+  },
+  getPortLikeCount(portid){
+    return firestore.collection(PORTFOLIOS).doc(portid).collection('likeList').get().then(snap => {
+      return snap.size // will return the collection size
+   });
+  },
+  getPortLikers(portid){
+    const portsCollection = firestore.collection(PORTFOLIOS).doc(portid).collection('likeList')
+    return portsCollection
+      .orderBy('created_at','desc')
+      .get()
+      .then((docSnapshots) => {
+        return docSnapshots.docs.map((doc) => {
+          let data = doc.data()
+          // Get firestore documentID
+          data.user = doc.id;
+          return data
+        })
+      })
+  },
+  async deletePostLike(postid, useremail){
+    await firestore.collection(POSTS).doc(postid).collection('likeList').doc(useremail).delete();
+    return true;
+  },
+  async addPostLike(postid, useremail){
+    await firestore.collection(POSTS).doc(postid).collection('likeList').doc(useremail).set({
+      created_at : firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    return true;
+  },
+  checkPostLike(postid, useremail){
+    const likes = firestore.collection(POSTS).doc(postid).collection('likeList').doc(useremail);
+    return likes.get()
+    .then(doc => {
+      if (doc.exists){
+        return true;
+      }
+      else{
+        return false;
+      }
+    })
+  },
+  getPostLikeCount(postid){
+    return firestore.collection(POSTS).doc(postid).collection('likeList').get().then(snap => {
+      return snap.size // will return the collection size
+   });
+  },
+  getPostLikers(postid){
+    const postsCollection = firestore.collection(POSTS).doc(postid).collection('likeList')
+    return postsCollection
+      .orderBy('created_at','desc')
+      .get()
+      .then((docSnapshots) => {
+        return docSnapshots.docs.map((doc) => {
+          let data = doc.data()
+          // Get firestore documentID
+          data.user = doc.id;
+          return data
+        })
+      })
+  },
   curUser() {
     var user = firebase.auth().currentUser;
     return user;
