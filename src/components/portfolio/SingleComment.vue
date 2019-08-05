7142ce3074931900e4868c2370b4bd7c4841b444
임현아 <ahlim721@gmail.com>
2019-08-05 11:55:52 +0900

    Add modify buttom with hover action if click submit, update database.collections

--- src/components/portfolio/SingleComment.vue
+++ src/components/portfolio/SingleComment.vue
@@ -6,20 +6,36 @@
     <div class="text">
       <div class="user">
         <a class="username body-1" href="#">{{nickname }}</a>
-        <span>{{ comment.text }}</span>
+        <span>
+          <span v-if="modYet">{{ comment.text }}</span>
+          <div v-if="!modYet">
+            <textarea id="mod-comment" :value="comment.text" rows=1></textarea>
+            <button class="caption" id="mod-comment-btn" @click="subComment">submit</button>
+          </div>
+        </span>
       </div>
       <div class="date caption">{{formatedDate()}}</div>
     </div>
-    <div class="del-btn" style="align-self:flex-end;">
-      <div v-if="this.$store.getters.getUser && comment.id === this.$store.getters.dbuser.email.split('@')[0]"
+    <div class="del-btn" style="align-self:flex-end; padding-bottom:15px;">
+      <span v-if="this.$store.getters.getUser && comment.id === this.$store.getters.dbuser.email.split('@')[0]"
+      v-on:click="showModifyTerminal"
+      style="cursor:pointer;"
+      @mouseenter="setModAfter()"
+      @mouseleave="setModBefore()">
+        <v-avatar tile size="25px">
+          <img :src="getImgUrl(2)" alt="avatar">
+        </v-avatar>
+      </span>
+      &nbsp;
+      <span v-if="this.$store.getters.getUser && comment.id === this.$store.getters.dbuser.email.split('@')[0]"
       v-on:click="deleteComment"
       style="cursor:pointer;"
       @mouseenter="setOpen()"
       @mouseleave="setClose()">
-        <v-avatar tile size="20px">
-          <img :src="getImgUrl()" alt="avatar">
+        <v-avatar tile size="28px">
+          <img :src="getImgUrl(1)" alt="avatar">
         </v-avatar>
-      </div>
+      </span>
     </div>
   </div>
 </template>
@@ -35,15 +51,34 @@ export default {
   data(){
     return {
       nickname:'',
-      trashicon:'closeTrash.png'
+      trashicon:'closeTrash.png',
+      modifyicon:'commentModBefore.png',
+      modYet: true,
     }
   },
   mounted(){
     this.getNickname(this.comment.id);
   },
   methods : {
-    getImgUrl() {
-      return require('@/assets/' + this.trashicon)
+    subComment() {
+      var modComment = document.getElementById("mod-comment").value;
+      firestore.collection('portfolios').doc(this.port.id).collection('commentList').doc(this.comment.key).update({
+        text : modComment
+      });
+      this.comment.text = modComment;
+      this.modYet = true;
+    },
+    setModAfter() {
+      this.modifyicon = 'commentModAfter.png'
+    },
+    setModBefore() {
+      this.modifyicon = 'commentModBefore.png'
+    },
+    getImgUrl(flag) {
+      if(flag == 1)
+        return require('@/assets/' + this.trashicon)
+      if(flag == 2)
+        return require('@/assets/' + this.modifyicon)
     },
     setOpen() {
       this.trashicon = 'openTrash.png'
@@ -57,7 +92,11 @@ export default {
         return `${date.getFullYear()}년 ${date.getMonth()+1}월 ${date.getDate()}일`
       }
     },
+    showModifyTerminal() {
+      this.modYet=!this.modYet;
+    },
     deleteComment(){
+      // swal - 삭제할 거냐고 물어보기
       firestore.collection('portfolios').doc(this.port.id).collection('commentList').doc(this.comment.key).delete()
       this.$emit('deleted',this.comment.key);
     },
@@ -92,7 +131,7 @@ export default {
   text-align: left;
   margin-left: 10px;
   align-self: flex-end;
-  width:80%;
+  width:78%;
 }
 .commemt .date {
   padding-top:2px;
@@ -102,6 +141,21 @@ export default {
   margin-right: 5px;
 }
 .commemt .del-btn {
-  align-self: flex-end;
+  /* align-self: flex-end; */
+}
+#mod-comment {
+  width:80%;
+  border-bottom: 1px solid #181818;
+}
+#mod-comment-btn {
+  display:inline-block;
+  width:14%;
+  margin: auto 3%;
+  font-weight: bold;
+  color:#ec407d;
+}
+#mod-comment-btn:hover {
+  color:#ffffff;
+  background: #ec407d;
 }
 </style>
