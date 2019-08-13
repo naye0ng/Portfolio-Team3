1d598992dc957a84c73fc814476a699209ff90ea
Park Haewon <hanaluana23@gmail.com>
2019-08-13 15:42:08 +0900

    Fix console log

--- src/components/post/PostList.vue
+++ src/components/post/PostList.vue
@@ -82,7 +82,7 @@ export default {
   },
   watch: {
     search : async function(newVal,oldVal){
-      console.log(newVal);
+      // console.log(newVal);
       if (!newVal){
         this.message = false;
       }
@@ -105,7 +105,7 @@ export default {
           this.message = true;
         }
       }
-      console.log(this.posts);
+      // console.log(this.posts);
     }
   }
   
